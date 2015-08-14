0c72b8bbafb3d0f453c561a6e0fcb8b19848c1b3
Vicente De Luca <vdeluca@zendesk.com>
2015-08-14 15:40:31 +0100

    adding grafana/influxdb support

--- script/pooler.rb
+++ script/pooler.rb
@@ -146,7 +146,7 @@ scheduler.every '5s' do
     info = ''
     response['target'].map {|k,v| info = info + "|#{k}(#{v})"}
     last_data = data
-    begin last_data[:values].delete(:series) rescue puts 'ok' end
+    begin last_data.delete(:series) rescue puts 'ok' end
     data = {
       values: { type: "WARNING", info: info.to_s, },
     }
@@ -157,7 +157,7 @@ scheduler.every '5s' do
     info = ''
     response['target'].map {|k,v| info = info + "|#{k}(#{v})"}
     last_data = data
-    begin last_data[:values].delete(:series) rescue puts 'ok' end
+    begin last_data.delete(:series) rescue puts 'ok' end
     data = {
       values: { type: "CRITICAL", info: info.to_s },
     }
