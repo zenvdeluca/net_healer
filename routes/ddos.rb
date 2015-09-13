4544850bb9a6ebd3f776b5a64225c36654853cc4
Vicente De Luca <vdeluca@zendesk.com>
2015-09-13 03:17:34 +0100

    adding features

--- routes/ddos.rb
+++ routes/ddos.rb
@@ -91,7 +91,8 @@ class Healer
       # normalize bps => mbps
 
       aggregate.each do |item|
-        puts item['incoming']['total']['traffic']
+        puts 'a'
+        #puts item['incoming']['total']['traffic']
         #item['incoming']['tcp']['traffic'] = item['incoming']['tcp']['traffic'].to_i / 1048576
         #item['incoming']['udp']['traffic'] = item['incoming']['udp']['traffic'].to_i / 1048576
         #item['incoming']['icmp']['traffic'] = item['incoming']['icmp']['traffic'].total_incoming_flows / 1048576
