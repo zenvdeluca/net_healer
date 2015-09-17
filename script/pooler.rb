0d3d73bbf295a8e9fb14b0e053db45e229a45dd9
Vicente De Luca <vdeluca@zendesk.com>
2015-09-17 22:41:15 +0100

    fixing in/out calculation
    fixing in/out calculation, when no outgoing data (division by 0 = infinite)

--- script/pooler.rb
+++ script/pooler.rb
@@ -199,11 +199,19 @@ end
 scheduler.every '5s' do
   total_bps = $influxdb_graphite.query "select last(value) from total where resource = 'bps' group by direction,resource"
   ratio_bps = total_bps[0]['values'].first['last'].to_f / total_bps[1]['values'].first['last'].to_f
-  payload_bps = { values: { info: ratio_bps } }
+  unless ratio_bps == Float::INFINITY
+    payload_bps = { values: { info: ratio_bps } }
+  else
+    payload_bps = { values: { info: 0 } }
+  end
 
   total_pps = $influxdb_graphite.query "select last(value) from total where resource = 'pps' group by direction,resource"
   ratio_pps = total_pps[0]['values'].first['last'].to_f / total_pps[1]['values'].first['last'].to_f
-  payload_pps = { values: { info: ratio_pps } }
+  unless ratio_pps == Float::INFINITY
+    payload_pps = { values: { info: ratio_pps } }
+  else
+    payload_pps = { values: { info: 0 } }
+  end
 
   $influxdb_events.write_point('ratio_bps', payload_bps)
   $influxdb_events.write_point('ratio_pps', payload_pps)
