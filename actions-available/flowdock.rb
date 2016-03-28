20481743ebb9684cbe46cae6dd501fba37f67975
Vicente De Luca <vdeluca@zendesk.com>
2016-03-28 01:05:37 +0100

    add critical flowdock notification

--- actions-available/flowdock.rb
+++ actions-available/flowdock.rb
@@ -23,5 +23,24 @@ class Actions
     return 'sleep'
   end
 
+  def critical_flowdock(current)
+    flows_token = AppConfig::FLOWDOCK.tokens
+    flows_token = flows_token.split(',')
+    info = current[:status].upcase + ' '
+    current[:target].map {|k,v| info = info + "|#{k}"}
+    if (Time.now - $lastpd) > 300 || (info != $lastinfo)
+      $lastpd = Time.now
+      $lastinfo = info
+      flows_token.each do |flow_token|
+        flow = Flowdock::Flow.new(:api_token => flow_token, :external_user_name => "NetHealer")
+        flow.push_to_chat(:content => "[CRITICAL] - Possible DDoS in #{AppConfig::NOTIFICATIONS.location} - target: #{info}", :tags => ["DDoS","Critical"])
+      end
+      puts "|Flowdock_Sent| - #{Time.now}"
+      return 'sent'
+    end
+    puts "|Flowdock_Sleep| - #{Time.now}"
+    return 'sleep'
+  end
+
 
 end
