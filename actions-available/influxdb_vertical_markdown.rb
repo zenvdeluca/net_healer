ddfa961affb435fd9ea03a0368f8d67b005afd3d
Vicente De Luca <vdeluca@zendesk.com>
2016-04-01 16:15:10 +0100

    moving influxdb vertical bar markdown as a plugin

---
+++ actions-available/influxdb_vertical_markdown.rb
@@ -0,0 +1,26 @@
+require ''
+
+
+class Actions
+  def warning_influx_vertical_mark(current)
+    info = current[:status].upcase + ' '
+    current[:target].map {|k,v| info = info + "|#{k}"}
+    data = {
+      values: { type: "WARNING", info: info.to_s, },
+    }
+    $influxdb_events.write_point('nethealer', data)
+    puts "|InfluxDB_Warning_Mark| - #{Time.now}"
+    return 'sent'
+  end
+
+  def critical_influx_vertical_mark(current)
+    info = current[:status].upcase + ' '
+    current[:target].map {|k,v| info = info + "|#{k}"}
+    data = {
+      values: { type: "CRITICAL", info: info.to_s, },
+    }
+    $influxdb_events.write_point('nethealer', data)
+    puts "|InfluxDB_Critical_Mark| - #{Time.now}"
+    return 'sent'
+  end
+end
--- script/pooler.rb
+++ script/pooler.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 require 'rufus-scheduler'
-require 'redis'
-require 'redis-namespace'
 require 'json'
 require 'dotenv'
 require 'rest-client'
@@ -10,22 +8,9 @@ require 'influxdb'
 Dotenv.load
 require_relative '../app_config'
 
-nethealer_server=AppConfig::NETHEALER.server
-#$influxdb_events = InfluxDB::Client.new 'events', host: AppConfig::NETHEALER.influxdb, username: AppConfig::NETHEALER.username, password: AppConfig::NETHEALER.password
 $influxdb_events = $influxdb_graphite = InfluxDB::Client.new 'graphite', host: AppConfig::NETHEALER.influxdb, username: AppConfig::NETHEALER.username, password: AppConfig::NETHEALER.password
 
-$redis_connection = Redis.new(:host => nethealer_server)
-$namespaced_current = Redis::Namespace.new('healer_current', redis: $redis_connection)
-$namespaced_history = Redis::Namespace.new('healer_history', redis: $redis_connection)
 scheduler = Rufus::Scheduler.new
-
-healer = RestClient::Resource.new(
-  "https://#{nethealer_server}/healer/v1/",
-  #user: Config::NETHEALER.user,
-  #password: Config::NETHEALER.password,
-  headers: { content_type: 'application/json' },
-  verify_ssl: false
-)
 
 $debug = 1
 
@@ -33,47 +18,9 @@ $debug = 1
 # Schedulers
 #
 
-# Graph vertical markdown. NET HEALER API query - Grafana: warning[yellow] & critical[red]
-
-last_data = nil
-data = ''
-
-scheduler.every '15s' do
-
-  response = JSON.parse(healer['ddos/status'].get)
-  status = response['status']
-  target = response['target']
-
-  case status
-  when 'clear'
-    print '!'
-  when 'warning'
-    puts "|Warning| - #{Time.now}"
-    info = ''
-    response['target'].map {|k,v| info = info + "|#{k}(#{v})"}
-    last_data = data
-    begin last_data.delete(:series) rescue puts 'ok' end
-    data = {
-      values: { type: "WARNING", info: info.to_s, },
-    }
-    $influxdb_events.write_point('nethealer', data) if data != last_data
-  else
-    puts "|Critical| - #{Time.now}"
-    info = ''
-    response['target'].map {|k,v| info = info + "|#{k}(#{v})"}
-    last_data = data
-    begin last_data.delete(:series) rescue puts 'ok' end
-    data = {
-      values: { type: "CRITICAL", info: info.to_s },
-    }
-    $influxdb_events.write_point('nethealer', data) if data != last_data
-  end
-
-end
-
 # Calculate in/out bps ratio -- consider refactor
 
-scheduler.every '15s' do
+scheduler.every '5s' do
   total_bps = $influxdb_graphite.query "select last(value) from total where resource = 'bps' group by direction,resource"
   ratio_bps = total_bps[0]['values'].first['last'].to_f / total_bps[1]['values'].first['last'].to_f
   unless ratio_bps == Float::INFINITY
