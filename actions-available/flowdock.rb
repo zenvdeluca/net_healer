8e3a60e557abea8f72d8574db718a5ab2d5e7a45
Vicente De Luca <vdeluca@zendesk.com>
2016-04-20 12:19:07 +0100

    fixing flowdock plugin

--- actions-available/flowdock.rb
+++ actions-available/flowdock.rb
@@ -15,7 +15,7 @@ class Actions
   @@grafana = AppConfig::GRAFANA.url
 
   def warning_flowdock(current)
-    info = '' ; @@lastinfo ||= ''
+    info = '' ; @@lastinfo ||= 'none'
     current[:target].map {|k,v| info = "|#{k}"}
     if (Time.now - $lastpd) > 300 || (info != @@lastinfo)
       $lastpd = Time.now
@@ -38,7 +38,7 @@ class Actions
   end
 
   def critical_flowdock(current)
-    info = '' ; @@lastinfo ||= ''
+    info = '' ; @@lastinfo ||= 'none'
     current[:target].map {|k,v| info = "|#{k}"}
     if (Time.now - $lastpd) > 300 || (info != @@lastinfo)
       $lastpd = Time.now
