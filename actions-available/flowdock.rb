d15dbc76ddad4e3461f872d9bb70a6415423139c
Vicente De Luca <vdeluca@zendesk.com>
2016-04-20 11:15:14 +0100

    improving flowdock message format

--- actions-available/flowdock.rb
+++ actions-available/flowdock.rb
@@ -1,7 +1,6 @@
 require 'flowdock'
 
 $lastpd = Time.now - 301
-$lastinfo = ''
 
 class Actions
 
@@ -10,23 +9,26 @@ class Actions
   @ping_count = 7
   @ping_timeout = 1
 
+  @flows_token = AppConfig::FLOWDOCK.tokens
+  @site = AppConfig::NOTIFICATIONS.location.downcase
+  @grafana = AppConfig::GRAFANA.url
+  @info = '' ; @lastinfo = ''
+
+
   def warning_flowdock(current)
-    flows_token = AppConfig::FLOWDOCK.tokens
-    site = AppConfig::NOTIFICATIONS.location.downcase
-    grafana = AppConfig::GRAFANA.url
-    info = current[:status].upcase + ' '
-    current[:target].map {|k,v| info = info + "|#{k}"}
-    if (Time.now - $lastpd) > 300 || (info != $lastinfo)
+    
+    current[:target].map {|k,v| @info = @info + "|#{k}"}
+    if (Time.now - $lastpd) > 300 || (@info != @lastinfo)
       $lastpd = Time.now
-      $lastinfo = info
-      flows_token.each do |flow_token|
+      @lastinfo = info
+      @flows_token.each do |flow_token|
         flow = Flowdock::Flow.new(:api_token => flow_token, :external_user_name => "NetHealer")
-        flow.push_to_chat(:content => ":warning: [NETHEALER-#{site.upcase}] - Possible DDoS Alert - target: #{info} \n- Graphs => #{grafana}/dashboard/db/#{site}-bps-pps-flows\n@team", :tags => ["DDoS","Warning"])
+        flow.push_to_chat(:content => ":warning: [#{@site.upcase}] - Possible DDoS Alert - target: #{@info} \n- Graphs => #{@grafana}/dashboard/db/#{@site}-bps-pps-flows\n@team", :tags => ["DDoS","Warning"])
         if @ping_enabled
           ping = `ping -c #{@ping_count} -W #{@ping_timeout} #{@ping_target} | grep -E "packet loss|min/avg/max"`.split("\n")
           loss = ping[0].split(', ')[2]
           min, avg, max, *discard = ping[1].split('= ')[1].split('/')
-          flow.push_to_chat(:content => "[PING-#{site.upcase} #{@ping_target}] - #{loss} - Latency: \n- min: #{min}ms, \n- avg: #{avg}ms, \n- max: #{max}ms", :tags => ["DDoS","Critical","Ping"])
+          flow.push_to_chat(:content => "[PING-#{@site.upcase} #{@ping_target}] - #{loss} - Latency: \n- min: #{min}ms, \n- avg: #{avg}ms, \n- max: #{max}ms", :tags => ["DDoS","Critical","Ping"])
         end
       end
       puts "|Flowdock_Sent| - #{Time.now}"
@@ -37,22 +39,19 @@ class Actions
   end
 
   def critical_flowdock(current)
-    flows_token = AppConfig::FLOWDOCK.tokens
-    site = AppConfig::NOTIFICATIONS.location.downcase
-    grafana = AppConfig::GRAFANA.url
-    info = current[:status].upcase + ' '
-    current[:target].map {|k,v| info = info + "|#{k}"}
-    if (Time.now - $lastpd) > 300 || (info != $lastinfo)
+    
+    current[:target].map {|k,v| @info = @info + "|#{k}"}
+    if (Time.now - $lastpd) > 300 || (@info != @lastinfo)
       $lastpd = Time.now
-      $lastinfo = info
-      flows_token.each do |flow_token|
+      @lastinfo = info
+      @flows_token.each do |flow_token|
         flow = Flowdock::Flow.new(:api_token => flow_token, :external_user_name => "NetHealer")
-        flow.push_to_chat(:content => ":death: [NETHEALER-#{site.upcase}] - Critical DDoS Alert - target: #{info} \n- Graphs => #{grafana}/dashboard/db/#{site}-bps-pps-flows\n@team", :tags => ["DDoS","Critical"])
+        flow.push_to_chat(:content => ":warning: [#{@site.upcase}] - Critical DDoS Alert - target: #{@info} \n- Graphs => #{@grafana}/dashboard/db/#{@site}-bps-pps-flows\n@team", :tags => ["DDoS","Warning"])
         if @ping_enabled
           ping = `ping -c #{@ping_count} -W #{@ping_timeout} #{@ping_target} | grep -E "packet loss|min/avg/max"`.split("\n")
           loss = ping[0].split(', ')[2]
           min, avg, max, *discard = ping[1].split('= ')[1].split('/')
-          flow.push_to_chat(:content => "[PING-#{site.upcase} #{@ping_target}] - #{loss} - Latency: \n- min: #{min}ms, \n- avg: #{avg}ms, \n- max: #{max}ms", :tags => ["DDoS","Critical","Ping"])
+          flow.push_to_chat(:content => "[PING-#{@site.upcase} #{@ping_target}] - #{loss} - Latency: \n- min: #{min}ms, \n- avg: #{avg}ms, \n- max: #{max}ms", :tags => ["DDoS","Critical","Ping"])
         end
       end
       puts "|Flowdock_Sent| - #{Time.now}"
@@ -61,6 +60,5 @@ class Actions
     puts "|Flowdock_Sleep| - #{Time.now}"
     return 'sleep'
   end
-
 
 end
